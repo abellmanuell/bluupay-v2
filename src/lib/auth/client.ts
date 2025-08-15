83707f5e8a8dcbbf99ee785634434d9de146d39b
abellmanuell <mannydev02@gmail.com>
2025-08-15 12:23:34 +0100

    Add baseURL and basePath

--- src/lib/auth/client.ts
+++ src/lib/auth/client.ts
@@ -1,14 +1,22 @@
-import { apiKeyClient, multiSessionClient, organizationClient, passkeyClient, twoFactorClient, usernameClient } from 'better-auth/client/plugins';
-import { createAuthClient } from 'better-auth/react'
+import {
+  apiKeyClient,
+  multiSessionClient,
+  organizationClient,
+  passkeyClient,
+  twoFactorClient,
+  usernameClient,
+} from "better-auth/client/plugins";
+import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: 'https://api-dev-v2.bluupay.co',
+  baseURL: "https://api-dev-v2.bluupay.co",
+  basePath: "/auth",
   plugins: [
     usernameClient(),
     apiKeyClient(),
     multiSessionClient(),
     organizationClient(),
     twoFactorClient(),
-    passkeyClient()
-  ]
-})
+    passkeyClient(),
+  ],
+});
--- src/lib/auth/server.ts
+++ src/lib/auth/server.ts
@@ -1,16 +1,23 @@
 // server-only client (import from /client for server use)
 import { createAuthClient } from "better-auth/client";
-import { usernameClient, apiKeyClient, multiSessionClient, organizationClient, passkeyClient } from "better-auth/client/plugins";
+import {
+  usernameClient,
+  apiKeyClient,
+  multiSessionClient,
+  organizationClient,
+  passkeyClient,
+} from "better-auth/client/plugins";
 import { twoFactorClient } from "better-auth/plugins/two-factor";
 
 export const authClientServer = createAuthClient({
-  baseURL: "http://localhost:3333",
+  baseURL: "https://api-dev-v2.bluupay.co",
+  basePath: "/auth",
   plugins: [
     usernameClient(),
     apiKeyClient(),
     multiSessionClient(),
     organizationClient(),
     twoFactorClient(),
-    passkeyClient()
-  ]
+    passkeyClient(),
+  ],
 });
